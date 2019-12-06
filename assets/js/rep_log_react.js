3e87af3ac0248a3c42f655ee48733a9cc0b4c0de
Arturas Silingas <arturas@silingas.lt>
2019-12-06 12:44:23 +0200

    add react

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,11 @@
-console.log('Hi react');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = React.createElement(
+    'h2',
+    null,
+    'Kėlimo istorija',
+    React.createElement('span', null, '!')
+);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
