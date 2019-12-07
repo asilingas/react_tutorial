b9aab132c65f620cb794b88ea0c91b9700b89adb
Arturas Silingas <arturas@silingas.lt>
2019-12-07 14:17:45 +0200

    add JSX

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = React.createElement(
-    'h2',
-    null,
-    'Kėlimo istorija',
-    React.createElement('span', null, '!')
-);
+const el = <h2>Lift Stuff! <span>❤️</span></h2>;
+
 
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
