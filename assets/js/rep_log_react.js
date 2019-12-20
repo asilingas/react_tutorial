5840e2e08b72342d11225a6b73b0984e74c774fd
Arturas Silingas <arturas@silingas.lt>
2019-12-20 13:34:39 +0200

    add JSX

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
+import RepLogApp from './RepLog/RepLogApp';
 
-const el = <h2>Lift Stuff! <span>❤️</span></h2>;
+const shouldShowHeart = true;
 
-
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+render(
+    <RepLogApp withHeart={shouldShowHeart} />,
+    document.getElementById('lift-stuff-app')
+);
